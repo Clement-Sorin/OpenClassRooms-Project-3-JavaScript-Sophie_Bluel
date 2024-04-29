29365ede05354741735d5746995b3c058c65b87a
Clement-Sorin <clement.sorin.141@gmail.com>
2024-04-29 15:55:04 +0200

    refactor code : deleted useless js files, modify the project generation on DOM mecanic to depend on filters only, add a class attribute to selected filters, make the first filter (tous/all) selected on page load

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,5 +1,7 @@
-import {genererProjets, genererFiltres, genererProjetsModale, toggleModale, openAjoutPhoto} from "./functions.js"
-import {verifierAuthentification} from "./authentified.js"
+const divProjets = document.querySelector(".gallery")
+const divFiltres = document.querySelector(".filtres")
+
+
 
 // Requête des données depuis l'API
 fetch("http://localhost:5678/api/works")
@@ -11,9 +13,50 @@ fetch("http://localhost:5678/api/works")
         return response.json()
     })
     .then(works => {
-        // DOM principal
-        genererProjets(works)
-        genererFiltres(works)
+        // Générer les filtres dans le DOM
+        const nomCategorie = new Set()
+        works.forEach(categories => {
+            nomCategorie.add(categories.category.name)
+        })
+        const tableCategories = Array.from(nomCategorie)
+        let filtreHTML = ""
+        filtreHTML = `<button id="${tableCategories}" class="btn-filtre">Tous</button>`
+        tableCategories.forEach(filtre => {
+            filtreHTML += `
+            <button id="${filtre}" class="btn-filtre">${filtre}</button>
+            `
+        })
+        divFiltres.innerHTML = filtreHTML
+
+        // Fonctionnalité des filtres
+        const btnFiltres = document.querySelectorAll(".btn-filtre")
+        btnFiltres.forEach(filtre => {
+            filtre.addEventListener("click", () => {
+                btnFiltres.forEach(btn => {
+                    btn.classList.remove("filter-selected")
+                })
+                filtre.classList.add("filter-selected")
+                const id = filtre.id
+                const categories = id.split(",")
+                const projetFiltres = works.filter(work => categories.includes(work.category.name))
+                let projetFiltresHTML = ""
+                projetFiltres.forEach(projet => {
+                    projetFiltresHTML += `
+                    <figure data-id="${projet.id}">
+                        <img src="${projet.imageUrl}" alt="${projet.title}">
+                        <figcaption>${projet.title}</figcaption>
+                    </figure>
+                    `
+                })
+                divProjets.innerHTML = projetFiltresHTML
+            })
+        })
+        // Selection du filtre "tous" en premier
+        for(let i=0; i<btnFiltres.length; i++) {
+            btnFiltres[i].click()
+            break
+        }
+
         // modale
         toggleModale()
         genererProjetsModale(works)
@@ -21,3 +64,242 @@ fetch("http://localhost:5678/api/works")
     })
 
 verifierAuthentification()
+
+
+function verifierAuthentification() {
+    const tokenLocalStorage = window.localStorage.getItem("token")
+    if (tokenLocalStorage) {
+        changerBoutonLogout()
+        genererBandeauEdit()
+        genererBoutonModifier()
+        cacherFiltres()
+    }
+}
+
+// Fonction de changement du bouton Login en Logout si authentifié
+function changerBoutonLogout() {
+    const logButton = document.getElementById("log")
+    logButton.innerText = "logout"
+    logButton.setAttribute("href", "")
+    logButton.addEventListener("click", () => {
+        window.localStorage.removeItem("token")
+    })
+}
+
+// Fonction d'apparition du bandeau du mode édition
+function genererBandeauEdit() {
+    const divEdit = document.getElementById("div-edit-hidden")
+    divEdit.id = "div-edit"
+}
+
+// Fonction d'apparition du bouton modifier
+function genererBoutonModifier() {
+    const divPortTitle = document.getElementById("modif-hidden") 
+    divPortTitle.id = "modif"
+}
+
+// Fonction cacher les filtres
+function cacherFiltres() {
+    const divFiltres = document.querySelector(".filtres")
+    divFiltres.classList.toggle("filtres-hidden")
+}
+
+function toggleModale() {
+    let modal = null
+    const openModal = function (e) {
+        e.preventDefault()
+        const target = document.querySelector(e.target.getAttribute("href"))
+        target.style.display = null
+        target.removeAttribute("aria-hidden")
+        target.setAttribute("aria-modal", "true")
+        modal = target
+        modal.addEventListener("click", closeModal)
+        modal.querySelector(".js-modal-close").addEventListener("click", closeModal)
+        modal.querySelector(".js-modal-stop").addEventListener("click", stopPropagation)
+    }
+
+    const closeModal = function (e) {
+        if(modal === null) return
+        e.preventDefault()
+        modal.style.display = "none"
+        modal.setAttribute("aria-hidden", "true")
+        modal.removeAttribute("aria-modal")
+        modal.removeEventListener("click", closeModal)
+        modal.querySelector(".js-modal-close").removeEventListener("click", closeModal)
+        modal.querySelector(".js-modal-stop").removeEventListener("click", stopPropagation)
+        modal = null
+    }
+
+    const stopPropagation = function(e) {
+        e.stopPropagation()
+    }
+    
+    document.querySelectorAll(".js-modal").forEach(a => {
+        a.addEventListener("click", openModal)
+    })
+}
+
+function genererProjetsModale(works) {
+    for (let i=0; i<works.length; i++) {
+        const projetIds = works[i].id
+        const projetImg = works[i].imageUrl
+        afficherGallery(projetImg, projetIds)
+        ecouterPoubelle(projetImg, projetIds)
+    }
+}
+
+// Fonction d'affichage des projets dans la modale
+const contentModale = document.getElementById("content-modal")
+const bodyModal = document.getElementById("body-modal")
+const titleModale = document.getElementById("title-modal")
+const boutonAjouter = document.getElementById("btn-modal")
+function afficherGallery(projetImg, projetIds) {
+    // Titre
+    titleModale.innerText = "Galerie Photo"
+    // Images
+    const divImage = document.createElement("div")
+    divImage.classList.add("div-image-modal")
+    const imgProjets = document.createElement("img")
+    imgProjets.src = projetImg
+    imgProjets.classList.add("img-modal")
+    contentModale.appendChild(divImage)
+    divImage.appendChild(imgProjets)
+    // bouton supprimer sur l'image
+    const linkIcon = document.createElement("a")
+    linkIcon.classList.add("link-icon-trash")
+    linkIcon.id = projetIds
+    linkIcon.innerHTML = `<i class="fa-solid fa-trash-can"></i>`
+    divImage.appendChild(linkIcon)
+    // Bouton
+    boutonAjouter.setAttribute("style", "display: block;")
+    boutonAjouter.innerText = "Ajouter une photo"
+}
+
+// Fonction de suppression du projet
+function ecouterPoubelle() {
+    // Recherche de l'ID du projet à supprimer
+    const trashLink = document.querySelectorAll(".link-icon-trash")
+    trashLink.forEach(trashLink => {
+        trashLink.addEventListener("click", () => {
+            const projetId = trashLink.id
+            envoyerDelete(projetId)
+        })
+    })
+}
+
+// Fonction de Requête DELETE des projets
+function envoyerDelete(projetId) {
+    // Pop Up de confirmation de suppression
+    let alertSuppression = confirm("Êtes-vous sûr de vouloir supprimer ce projet?")
+    if(alertSuppression) {
+        // Préparation des données pour la requête
+        const token = localStorage.getItem("token")
+        const headerDelete = {
+            "Authorization": "Bearer " + token
+        }
+        // Requête fetch avec method DELETE
+        try {
+            fetch(`http://localhost:5678/api/works/${projetId}`, {
+                method: "DELETE",
+                headers: headerDelete,
+                body: projetId
+            }).then(response => {
+                if (response.ok === false) {
+                    alert("Erreur de la requête HTTP")
+                } else {
+                    rafraichirModale()
+                }
+            })
+        } catch(error) {
+            console.error(error)
+            alert("Le projet n'a pas pu être supprimé")
+        }
+    } 
+}
+
+// Fonction de rafraîchissement de la modale
+function rafraichirModale() {
+    contentModale.innerHTML = ""
+    divProjets.innerHTML = ""
+    fetch("http://localhost:5678/api/works")
+        .then(response => {
+            if (response.ok === false) {
+                throw new Error("Erreur de la requête HTTP")
+            }
+            return response.json()
+        })
+        .then(works => {
+            genererProjetsModale(works)
+            genererProjets(works)
+        })
+}
+
+// Fonction d'ouverture de la modale partie 2 (Ajout Photo)
+export function openAjoutPhoto() {
+    boutonAjouter.addEventListener("click", (e) => {
+        e.preventDefault()
+        bodyModal.innerHTML= ""
+        afficherAjoutPhoto()
+    })
+}
+
+// Fonction d'ajout du contenu sur la partie Ajout Photo de la modale
+export function afficherAjoutPhoto() {
+    titleModale.innerText = "Ajout photo"
+    // Icon Retour
+    const divIconRetour = document.getElementById("div-icon-fermer")
+    divIconRetour.innerHTML += `<a id="icon-retour"><i class="fa-solid fa-arrow-left"></i></a>`
+    divIconRetour.setAttribute("style","flex-direction: row-reverse; justify-content: space-between;")
+    retournerModaleGallerie()
+    // Form
+    const formAjout = document.createElement("form")
+    formAjout.id = "form-ajout-photo"
+    bodyModal.appendChild(formAjout)
+    // Input Ajouter photo
+    const divAjouterPhoto = document.createElement("div")
+    divAjouterPhoto.id = "div-ajouter-photo"
+    formAjout.appendChild(divAjouterPhoto)
+    divAjouterPhoto.innerHTML = `<i class="fa-regular fa-image"></i>`
+    divAjouterPhoto.innerHTML += `<button id="btn-input-file"><label for="file-input" class="custom-file-input">+ Ajouter Photo</label><input type="file" id="file-input" required></button>`
+    divAjouterPhoto.innerHTML += `<p>jpg, png: 4mo max</p>`
+    // Input titre
+    const divTitreAjouterPhoto = document.createElement("div")
+    divTitreAjouterPhoto.id = "div-titre-ajouter-photo"
+    formAjout.appendChild(divTitreAjouterPhoto)
+    divTitreAjouterPhoto.innerHTML = `<label for="input-titre-ajouter-photo">Titre</label>`
+    divTitreAjouterPhoto.innerHTML += `<input type="text" id="input-titre-ajouter-photo" name="title" required>`
+    // Input catégorie
+    const divCategorieAjouterPhoto = document.createElement("div")
+    divCategorieAjouterPhoto.id = "div-categorie-ajouter-photo"
+    formAjout.appendChild(divCategorieAjouterPhoto)
+    divCategorieAjouterPhoto.innerHTML = `<label for="categorie-ajouter-photo">Catégorie</label>`
+    divCategorieAjouterPhoto.innerHTML += `<select name="category" id="input-categorie-ajouter-photo" required></select>`
+    const selectCategories = document.getElementById("input-categorie-ajouter-photo")
+    selectCategories.innerHTML += `<option value="" disabled selected style="display: none;"></option>`
+    fetch("http://localhost:5678/api/categories")
+    .then(response => {
+        if (response.ok === false) {
+            throw new Error("Erreur de la requête HTTP")
+        }
+        return response.json()
+    })
+    .then(data => {
+        data.forEach(objet => {
+            const id = objet.id
+            const name = objet.name
+            selectCategories.innerHTML += `<option value="${id}">${name}</option>`
+        })
+    })
+    // Button Submit
+    const divBoutonAjouterPhoto = document.createElement("div")
+    divBoutonAjouterPhoto.id = "div-bouton-ajouter-photo"
+    formAjout.appendChild(divBoutonAjouterPhoto)
+    divBoutonAjouterPhoto.innerHTML = `<button type="submit" id="btn-valider-ajouter-photo">Valider</button>`
+}
+
+function retournerModaleGallerie() {
+    const boutonRetour = document.getElementById("icon-retour")
+    boutonRetour.addEventListener("click", () => {
+        bodyModal.innerHTML= ""
+    })
+}
