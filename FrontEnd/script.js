cb0e20bb4b9b6f9a503c94ade60707d31f686285
Clement-Sorin <clement.sorin.141@gmail.com>
2024-04-29 17:46:43 +0200

    defactor code: toggle modal = change the aside HTML tag to dialog and use showModal() and close() function in JS

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -4,6 +4,9 @@ const logButton = document.getElementById("log")
 const token = window.localStorage.getItem("token")
 const divEdit = document.getElementById("div-edit-hidden")
 const divPortTitle = document.getElementById("modif-hidden")
+const modal = document.getElementById("modal")
+const linkOpenModal = document.getElementById("link-modif")
+const linkCloseModal = document.getElementById("icon-fermer")
 
 // Requête des données depuis l'API
 fetch("http://localhost:5678/api/works")
@@ -60,7 +63,6 @@ fetch("http://localhost:5678/api/works")
         }
 
         // modale
-        toggleModale()
         genererProjetsModale(works)
         openAjoutPhoto()
     })
@@ -76,40 +78,14 @@ if (token) {
     divFiltres.classList.toggle("filtres-hidden")
 }
 
-function toggleModale() {
-    let modal = null
-    const openModal = function (e) {
-        e.preventDefault()
-        const target = document.querySelector(e.target.getAttribute("href"))
-        target.style.display = null
-        target.removeAttribute("aria-hidden")
-        target.setAttribute("aria-modal", "true")
-        modal = target
-        modal.addEventListener("click", closeModal)
-        modal.querySelector(".js-modal-close").addEventListener("click", closeModal)
-        modal.querySelector(".js-modal-stop").addEventListener("click", stopPropagation)
+// Ouverture et fermeture de la modale
+linkOpenModal.addEventListener("click", () => {modal.showModal()})
+linkCloseModal.addEventListener("click", () => {modal.close()})
+window.addEventListener("click", event => {
+    if(modal.open) {
+        if(event.target === modal) {modal.close()}
     }
-
-    const closeModal = function (e) {
-        if(modal === null) return
-        e.preventDefault()
-        modal.style.display = "none"
-        modal.setAttribute("aria-hidden", "true")
-        modal.removeAttribute("aria-modal")
-        modal.removeEventListener("click", closeModal)
-        modal.querySelector(".js-modal-close").removeEventListener("click", closeModal)
-        modal.querySelector(".js-modal-stop").removeEventListener("click", stopPropagation)
-        modal = null
-    }
-
-    const stopPropagation = function(e) {
-        e.stopPropagation()
-    }
-    
-    document.querySelectorAll(".js-modal").forEach(a => {
-        a.addEventListener("click", openModal)
-    })
-}
+})
 
 function genererProjetsModale(works) {
     for (let i=0; i<works.length; i++) {
